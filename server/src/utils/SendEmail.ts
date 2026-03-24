9cc45dbc135569a7d85dc08ece1d3f0383f17046
Ritik Rathour <ritikrathour73@gmail.com>
2026-03-25 00:38:35 +0530

    change env

--- server/src/utils/SendEmail.ts
+++ server/src/utils/SendEmail.ts
@@ -1,14 +1,13 @@
 import nodemailer from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
-import { config } from "../config";
 // Email transporter setup
 const mailOptions: SMTPTransport.Options = {
-  host: config.emailHost,
-  port: Number(config.emailPort),
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
   secure: false,
   auth: {
-    user: config.emailFrom,
-    pass: config.emailpass,
+    user: process.env.EMAIL_FROM,
+    pass: process.env.EMAIL_Pass,
   },
 };
 const Transporter = nodemailer.createTransport(mailOptions);
@@ -16,7 +15,7 @@ const Transporter = nodemailer.createTransport(mailOptions);
 const SendEmail = async (to: string, subject: string, html: string) => {
   try {
     await Transporter.sendMail({
-      from: config.emailFrom,
+      from: process.env.EMAIL_FROM,
       to,
       subject,
       html,
