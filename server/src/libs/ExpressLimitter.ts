59d85df695b0c8ceb2fb72f857d7bb19d92b94f4
Ritik Rathour <ritikrathour73@gmail.com>
2026-03-28 00:04:38 +0530

    update the auth limitter

--- server/src/libs/ExpressLimitter.ts
+++ server/src/libs/ExpressLimitter.ts
@@ -14,7 +14,7 @@ export const generalLimiter = rateLimit({
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 6, // limit each IP to 5 requests per windowMs
+  max: 10, // limit each IP to 5 requests per windowMs
   message: {
     error: "Too many authentication attempts, please try again later.",
     retryAfter: Math.ceil((5 * 60 * 1000) / 1000),
