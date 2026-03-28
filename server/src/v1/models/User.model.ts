841b35490dca8c48abaf377298bcb6946595908d
Ritik Rathour <99650054+ritikrathour@users.noreply.github.com>
2026-03-28 14:49:13 +0530

    Fix avatar default value formatting in User model

--- server/src/v1/models/User.model.ts
+++ server/src/v1/models/User.model.ts
@@ -41,8 +41,7 @@ const UserSchema: mongoose.Schema<IUser> = new mongoose.Schema<IUser>(
     phone: String,
     avatar: {
       type: String,
-      default:
-        "https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3383.jpg?semt=ais_incoming&w=740&q=80",
+      default:"https://img.freepik.com/premium-vector/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-vector-illustration_561158-3383.jpg?semt=ais_incoming&w=740&q=80"
     },
     refreshToken: {
       type: String,
